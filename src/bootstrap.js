d4f1b5f871ae2b583a0ce7e0106a7c10b09b8727
MACHADO VIEIRA Edson <edson.machado@soprasteria.com>
2021-06-02 18:28:05 +0200

    consuming remote modules

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,10 +1,34 @@
 import faker from 'faker';
 
-let products = '';
+const mount = (el) => {
+  let products = '';
 
-for (let i = 0; i < 5; i++) {
-  const name = faker.commerce.productName();
-  products += `<div>${name}</div>`;
+  for (let i = 0; i < 5; i++) {
+    const name = faker.commerce.productName();
+    products += `<div>${name}</div>`;
+  }
+
+  el.innerHTML = products;
+};
+
+// Context/Situation #1
+// We are running this file in development in isolation
+// We are using our local index.html file
+// Which definitely has an element with an id of 'dev-products'
+// We want to immediately render our app into that element
+if (process.env.NODE_ENV === 'development') {
+  const el = document.querySelector('#dev-products');
+  // Assuming out container doesn't have an element
+  if (el) {
+    // We are probably running in isolation
+    mount(el);
+  }
 }
 
-document.querySelector('#dev-products').innerHTML = products;
+// Context/Situation #2
+// We are running this file in development or production
+// through the container app
+// No guarantee that an element with an id of 'dev-products'
+// We do not want to immediately render our app
+
+export {mount};
